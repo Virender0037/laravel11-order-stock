4890a3383cfb01ad1c11c27da109e6e9fc7dc106
Virender Singh <dk77089@gmail.com>
2026-02-24 18:46:52 +0530

    feature test

--- tests/Feature/CustomerFeatureTest.php
+++ tests/Feature/CustomerFeatureTest.php
@@ -10,13 +10,66 @@ use Tests\TestCase;
 
 class CustomerFeatureTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    use RefreshDatabase;
+
     public function test_guest_cannot_access_customers_index(): void
     {
-        $response = $this->get('/customers');
+        $response = $this->get('/customers')
+                 ->assertRedirect('/login');
+    }
 
-        $response->assertRedirect('/login');
+    public function test_auth_user_can_view_customers_index(): void
+    {
+        $user = User::factory()->create();
+            
+        $this->actingAs($user)->get('/customers')->assertOk();
+    }
+
+    public function test_auth_user_can_create_customers(): void
+    {   
+        $user = User::factory()->create();
+
+        $customerdata = Customer::factory()->make()->toArray();
+
+        $this->actingAs($user)
+         ->post('/customers', $customerdata)
+         ->assertRedirect('/customers');
+
+        $this->assertDatabaseHas('customers', [
+            'email' => $customerdata['email'],
+        ]);
+
+    }
+
+    public function test_duplicate_emails(): void
+    {
+
+    }
+
+    public function test_auth_user_can_update_customers(): void
+    {   
+        $customer = Customer::factory()->create();
+
+        $user = User::factory()->create();
+
+        $updateddata = Customer::factory()->make()->toArray();
+
+        $this->actingAs($user)
+         ->put("/customers/{$customer->id}", $updateddata);
+        
+        $this->assertDatabaseHas('customers', [
+            'id' => $customer->id,
+            'email' => $updatedData['email'],
+        ]);
+
+    }
+
+    public function test_auth_user_can_delete_customers(): void
+    {   
+        $customer = Customer::factory()->create();
+
+        $user = User::factory()->create();
+
+
     }
 }
